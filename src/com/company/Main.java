c4a32ca0a26e5a5119e61b9510ad56a10b3467ef
Paul <paul.torrillo@gmail.com>
2016-10-27 16:22:01 -0400

    hi

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,13 +3,19 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-	int grades[] = new int[1000];
+        int grades[] = new int[1000];
+        String grade[] = new String[1000];
         Scanner user = new Scanner(System.in);
         System.out.println();
         int h = 0;
-        System.out.println("Enter grade or exit to exit.");
-        while (true)
-        {
+        System.out.println("Enter grade and name or exit to exit.");
+        while (true) {
+            System.out.println("Enter name");
+            String y = user.nextLine();
+            if (y.equalsIgnoreCase("Exit"))
+                break;
+            grade[h] = y;
+            System.out.println("Enter grade");
             String z = user.nextLine();
             if (z.equalsIgnoreCase("Exit"))
                 break;
@@ -17,10 +23,12 @@ public class Main {
             h++;
         }
         int b = 0;
-        for (int j = 0; j < h; j++ )
-        {
-            b+= grades[j];
+        for (int j = 0; j < h; j++) {
+            b += grades[j];
         }
-        System.out.println("Average: " + b/h);
+        System.out.println("Average: " + b / h);
+        for (int j = 0; j < h; j++) {
+            System.out.println(grade[j]);
+        }
     }
 }
